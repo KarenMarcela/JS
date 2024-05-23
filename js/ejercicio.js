f247a3e78ca3eaed459d8f665a42c2fbe5ac0aa8
KarenMarcela <kmbayonam@gmail.com>
2024-05-22 21:37:43 -0500

    KarenMarcela

---
+++ js/ejercicio.js
@@ -0,0 +1,9 @@
+
+let num1 = document.getElementById("num1").value  ;
+let num2 = document.getElementById("num2").value  ;
+var array_num=[1,2,3,4,5,6,7,8,9,10];
+document.getElementById("array").innerText ="El array a calcular es : "+array_num ;
+function  enviar(){
+    alert(""+num1);
+}
+
